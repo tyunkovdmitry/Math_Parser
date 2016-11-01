9f0d7aa7b9787d446d5f796387e4f13e3b3aa689
Dmitry <tyunkov.dima@gmail.com>
2016-11-01 14:58:31 +0600

    Initial commit. Full first version of application.
    Signed-off-by: Dmitry <tyunkov.dima@gmail.com>

---
+++ MathParserDLL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("A library for converting an input mathematical expressions in the output value.")]
+[assembly: AssemblyDescription("A library for converting an input mathematical expressions in the output value.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mathematical Parser")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2014, Tyunkov Dmitry")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(true)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("65cafcfa-b2a8-4c82-bc26-5ce1aad317ff")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.3.1.1538")]
+[assembly: AssemblyFileVersion("1.3.1.1538")]
